be561e6a6b23b04bca62d21a7e1a0e4a06e14328
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-16 16:27:32 +0700

    feat/we

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,36 @@ function getCurrentDateTime() {
 }
 
 function IntroSection() {
+  const articleStyles = {
+    container: "article",
+    title: "article-title",
+    body: "article-body",
+    link: "article-link",
+  };
+
   return (
-    <div>
-      <h3>TechUp Thailand</h3>
-      <p>เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน</p>
-      <a href="https://www.techupth.com/" target="blank">
+    <div className={articleStyles.container}>
+      <h3 className={articleStyles.title}>TechUp Thailand</h3>
+      <p className={articleStyles.body}>
+        เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน
+      </p>
+      <a
+        className={articleStyles.link}
+        href="https://www.techupth.com/"
+        target="blank"
+      >
         techupth.com
       </a>
-      {/* ให้แสดงวันเวลาด้วย Function `getCurrentDateTime` ที่กำหนดไว้ให้ */}
+      <div className="bootcamp-start-time ">
+        วันเวลาเริ่มหลักสูตร คือ (วันเวลา){getCurrentDateTime()}
+      </div>
     </div>
   );
 }
 
 function LoginForm() {
   return (
-    <div class="login-container">
+    <div class="login-container ">
       <form class="login-form">
         <h2>Login</h2>
         <label for="username">Username</label>
